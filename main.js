a6a01ace58bd25bc9a0cc89406e7514688247a5f
mahda07 <mahdafatihah@gmail.com>
2025-02-24 10:34:23 +0700

    menampilkan daftar pelanggan

--- main.js
+++ main.js
@@ -127,3 +127,25 @@ export async function ambilDaftarBarangDiKeranjang() {
 export async function  hapusBarangDariKeranjang(id){
   await deleteDoc(doc(basisdata, "transaksi",id))
 }
+
+export async function ambilDaftarPelanggan() {
+  const refDokumen = collection(basisdata, "pelanggan");
+  const kueri = query(refDokumen, orderBy("nama"));
+  const cuplikanKueri = await getDocs(kueri);
+
+  let hasilKueri = [];
+  cuplikanKueri.forEach((dokumen) => {
+    hasilKueri.push({
+      id: dokumen.id,
+      nama: dokumen.data().nama,
+      alamat: dokumen.data().alamat,
+      nohape: dokumen.data().nohape
+    })
+  })
+
+  return hasilKueri;
+}
+export async function  hapusDaftarPelanggan(id){
+  await deleteDoc(doc(basisdata, "pelanggan",id))
+}
+
